3862ea76a85a1344526b589e1c240cca925ecd46
Vanessa Nascimento <vanessa_nascimento03@hotmail.com>
2018-12-11 00:54:57 -0200

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include('pages/header.php'); ?>
+
+<?php include('pages/menu.php'); ?>
+
+
+<div class="parallax-container">
+    <div class="parallax"><img src="images/parallax1.jpg"></div>
+</div>
+
+
+<?php include('pages/footer.php'); ?>
